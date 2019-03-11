7b9d8cd0bc70cce977f26c9c0c80fd54c6dc9647
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-03-11 10:44:37 +0100

    Przygotowanie do Testów

---
+++ DevHobby.Common/EmailSevice.cs
@@ -0,0 +1,25 @@
+﻿namespace DevHobby.Common
+{
+    public class EmailSevice
+    {
+
+        /// <summary>
+        /// Wysyła wiadomosc email.
+        /// </summary>
+        /// <param name="temat">Temat wiadomosci.</param>
+        /// <param name="wiadomosc">Widomosc tekstowa</param>
+        /// <param name="odbiorca">Adres email odbiorcy wiadomosci</param>
+        /// <returns></returns>
+        public string WyslijWiadomosc(string temat, string wiadomosc, string odbiorca)
+        {
+            // Kod, aby wysłać wiadomosc email
+
+
+            var potwierdzenie = "Wiadomość wysłana: " + temat;
+            var logowanieService = new LogowanieService();
+            logowanieService.Logowanie(potwierdzenie);
+
+            return potwierdzenie;
+        }
+    }
+}
