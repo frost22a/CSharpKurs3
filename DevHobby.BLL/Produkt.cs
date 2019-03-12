43da5a734b5a7b7fbdc5f18c2b9cfde2540642f5
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-03-12 11:52:55 +0100

    Budowanie Klasy Produkt

---
+++ DevHobby.BLL/Produkt.cs
@@ -0,0 +1,37 @@
+﻿namespace DevHobby.BLL
+{
+    /// <summary>
+    /// Zarzadza produktami
+    /// </summary>
+    public class Produkt
+    {
+        private int produktId;
+
+        public int ProduktId
+        {
+            get { return produktId; }
+            set { produktId = value; }
+        }
+
+        private string nazwaProduktu;
+
+        public string NazwaProduktu
+        {
+            get { return nazwaProduktu; }
+            set { nazwaProduktu = value; } 
+        }
+
+        private string opis;
+
+        public string Opis
+        {
+            get { return opis; }
+            set { opis = value; }
+        }
+
+        public string PowiedzWitaj()
+        {
+            return "Witaj " + NazwaProduktu + " (" + ProduktId + "): " + Opis;
+        }
+    }
+}
---
+++ DevHobbyBLLTesty/ProduktTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevHobby.BLL.Tests
+{
+    [TestClass()]
+    public class ProduktTests
+    {
+        [TestMethod()]
+        public void PowiedzWitajTest()
+        {
+            // Arrange (zaranżuj test)
+            var produkt = new Produkt();
+            produkt.ProduktId = 1;
+            produkt.NazwaProduktu = "Biurko";
+            produkt.Opis = "Czerwone biurko";
+            var oczekiwana = "Witaj Biurko (1): Czerwone biurko";
+
+            //ACT (działaj)
+            var aktualna = produkt.PowiedzWitaj();
+
+            // Assert (potwierdz test)           
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+    }
+}
